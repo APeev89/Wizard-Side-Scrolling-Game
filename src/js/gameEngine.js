bc392b1fc612d14e5fb5613451d12fdd4a417700
APeev89 <apostol.peev@gmail.com>
2022-02-16 21:51:28 +0200

    Create wizard with initial position on start

--- src/js/gameEngine.js
+++ src/js/gameEngine.js
@@ -1,9 +1,10 @@
 function start(state, game) {
+    game.createWizard(state.wizard);
+
     window.requestAnimationFrame(gameLoop.bind(null,state,game));
 }
 
 function gameLoop(state,game) {
-    console.log('frame');
 
     window.requestAnimationFrame(gameLoop.bind(null,state,game));
 }
