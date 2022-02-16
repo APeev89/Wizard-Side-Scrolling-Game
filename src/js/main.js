5f8bce54c7a361251c182ac8be02545c69e374d5
APeev89 <apostol.peev@gmail.com>
2022-02-16 21:17:26 +0200

    Separate login into different files

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+let state = initState();
+let game = initGameObject();
+
+game.startScreen.addEventListener('click',(e)=>{
+    game.gameScreen.classList.add('hidden');
+    game.gameScreen.classList.remove('hidden');
+
+    
+});
